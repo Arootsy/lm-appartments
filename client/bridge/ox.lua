8455db1756efc7a2970855a592e127eae22c1ad6
Arootsy <aronrootsaert07@gmail.com>
2024-12-22 20:55:36 +0100

    feat(ox): ox support added

--- client/bridge/ox.lua
+++ client/bridge/ox.lua
@@ -1,5 +1,3 @@
 if not Framework.Ox() then return end
 
-local Ox = require '@ox_core.lib.init'
-
 Framework.OnPlayerLoaded = 'ox:playerLoaded'
--- server/bridge/ox.lua
+++ server/bridge/ox.lua
@@ -1,3 +1,19 @@
 if not Framework.Ox() then return end
 
 local Ox = require '@ox_core.lib.init'
+
+function Framework.GetPlayer(src)
+    local Player = Ox.GetPlayer(src)
+
+    if Player then
+        return Player
+    end
+end
+
+function Framework.GetPlayerFromIdentifier(identifier)
+    local Player = Ox.GetPlayerByIdentifier({ identifier = identifier })
+
+    if Player then
+        return Player
+    end
+end
