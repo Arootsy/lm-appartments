e58b5b8a03a8e046a9b448dd5b22873ebe56eeec
Arootsy <115181211+Arootsy@users.noreply.github.com>
2024-12-29 18:20:49 +0100

    fix(sql): description column not necessary

--- init.sql
+++ init.sql
@@ -2,7 +2,6 @@ CREATE TABLE `owned_appartments` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `owner` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
     `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    `description` text COLLATE utf8_unicode_ci NOT NULL,
     `price` int(11) NOT NULL,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
