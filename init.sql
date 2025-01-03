428fad55196de7bed6de7eec5b676275fb16800c
Arootsy <aronrootsaert07@gmail.com>
2025-01-03 19:43:22 +0100

    fix(sql): check if owned appartments exists

--- init.sql
+++ init.sql
@@ -1,7 +1,23 @@
-CREATE TABLE `owned_appartments` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `owner` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-    `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    `price` int(11) NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `owned_appartments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` varchar(50) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `rent` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+ALTER TABLE `owned_appartments`
+    ADD COLUMN IF NOT EXISTS `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST,
+    ADD COLUMN IF NOT EXISTS `owner` varchar(50) NOT NULL,
+    ADD COLUMN IF NOT EXISTS `name` varchar(255) NOT NULL,
+    ADD COLUMN IF NOT EXISTS `price` int(11) NOT NULL,
+    ADD COLUMN IF NOT EXISTS `rent` int(11) DEFAULT NULL,
+    ADD COLUMN IF NOT EXISTS `created_at` timestamp NOT NULL DEFAULT current_timestamp();
+
+ALTER TABLE `owned_appartments`
+    ENGINE=InnoDB 
+    AUTO_INCREMENT=4 
+    DEFAULT CHARSET=utf8mb3 
+    COLLATE=utf8mb3_unicode_ci;
